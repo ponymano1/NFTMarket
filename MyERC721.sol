40513bbbe7a8c4b980eaced404b05082815a1b9f
zack <xiaomano1@outlook.com>
2024-01-15 23:02:07 +0800

    NFTMarket

---
+++ MyERC721.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+//import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract MyERC721 is ERC721URIStorage {
+    uint256 private tokenIds;
+
+    constructor (string memory name_, string memory symbol_) ERC721(name_, symbol_) {
+    }
+
+    function mint(address to, string memory tokenURI) public returns (uint256) {
+        tokenIds++;
+        uint256 tokenId = tokenIds;
+        _mint(to, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+        return tokenId;
+    }
+
+}
